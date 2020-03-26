ea805110513ba96343d8b9f8b70f363d1586885d
Emily Richardson <richardson96@gmail.com>
2020-03-26 02:13:09 -0500

    Angular exercises part 1 done

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'My Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://lh3.googleusercontent.com/proxy/gs8SOZSUclTymqe7OFDnIrRG8Vb5JgceaiQUqBm9iKTs7nODoQr-kvH3wasM1NYZquhdCQJBquAmDY5L6dw1VDDahQ';
+  image3 = 'https://12ax7web.s3.amazonaws.com/accounts/1/products/1986199880298/I-Did-Nothing-Today_800x800_SEPS-1000x1000.jpg';
 
   constructor() { }
 
